1fcc1cd07fdae1a16efc5527d2eae9607da99e8b
Takatoshi Hino <like.the.wind0306@gmail.com>
2019-12-11 00:22:37 +0900

    Macの基本セッティング一部と参考資料追記

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# ================================================================================
+#   Setup MacOs
+# ================================================================================
+
+if [ "$(uname)" != "Darwin" ] ; then
+  echo 'Hmmm, its Not MacOS!'
+  exit 1
+fi
+
+echo 'Setup MacOS!'
+
 # Command Line Tools for Xcode
 xcode-select --install
 
@@ -11,3 +22,17 @@ if [ ! -x "`which brew`" ] ; then
   brew upgrade
   brew -v
 fi
+
+
+# Macbook本体のセッティング
+chflags nohidden ~/Library    # ~/Library ディレクトリを可視化
+sudo chflags nohidden /Volumes    # /Volumes ディレクトリを可視化
+sudo nvram SystemAudioVolume=" "    # ブート時のサウンド無効化
+
+echo 'Congrats!! Yout are all set!'
+
+
+# 参考記事
+# http://neos21.hatenablog.com/entry/2019/01/10/080000 (defaultsコマンド)
+# https://qiita.com/kai_kou/items/af5d0c81facc1317d836 (setup.shまとめ)
+# https://qiita.com/kai_kou/items/3107e0a056c7a1b569cd (環境構築系記事)
