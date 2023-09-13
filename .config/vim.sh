220f073c7a0294a214c5bc7327cad232c5c4ccea
t_hino <t_hino@gizumo-admin-01noMacBook-Pro.local>
2023-09-14 03:39:40 +0900

    refactor: vim.sh

--- .config/vim.sh
+++ .config/vim.sh
@@ -4,11 +4,11 @@ source "$(cd "$(dirname "$0")" && pwd)/colors.txt"
 
 echo "${CYAN}INFO: Installing Vundle and plugins${ESC_END}"
 
-if [ ! -d ~/.vim/bundle ] ; then
-  mkdir -p ~/.vim/bundle/Vundle.vim
+if [ ! -d "${HOME}/.vim/bundle" ] ; then
+  mkdir -p "${HOME}/.vim/bundle/Vundle.vim"
 fi
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
 vim +PluginInstall +qall
 
 echo "${GREEN}INFO: Done${ESC_END}"
