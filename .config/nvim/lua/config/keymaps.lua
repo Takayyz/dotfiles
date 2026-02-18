8f735da621d897c883c89a10ddf40f5108e4539d
Takayyz <like.the.wind0306@gmail.com>
2026-02-18 23:35:06 +0900

    feat: add keybindings for neovim
    - jj to escape in insert mode
    - Leader+w/q for save/quit
    - Leader+h/l for line beginning/end
    - Leader+c for comment toggle
    - Leader+</> for indent/dedent with visual selection support

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,27 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+local map = vim.keymap.set
+
+-----------------------------------
+-- Insert mode
+-----------------------------------
+map("i", "jj", "<Esc>", { desc = "Escape" })
+
+-----------------------------------
+-- Normal mode
+-----------------------------------
+map("n", "<Leader>w", "<Cmd>w<CR>", { desc = "Save" })
+map("n", "<Leader>q", "<Cmd>q<CR>", { desc = "Quit" })
+map("n", "<Leader>h", "^", { desc = "Go to beginning of line" })
+map("n", "<Leader>l", "$", { desc = "Go to end of line" })
+map("n", "<Leader>c", "gcc", { remap = true, desc = "Toggle comment" })
+map("n", "<Leader><", "<<", { desc = "Dedent line" })
+map("n", "<Leader>>", ">>", { desc = "Indent line" })
+
+-----------------------------------
+-- Visual mode
+-----------------------------------
+map("v", "<Leader>c", "gc", { remap = true, desc = "Toggle comment" })
+map("v", "<Leader><", "<gv", { desc = "Dedent and keep selection" })
+map("v", "<Leader>>", ">gv", { desc = "Indent and keep selection" })
