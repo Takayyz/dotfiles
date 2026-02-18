0f4937a71681dfc818b865de9be86bea0c77358f
Takayyz <like.the.wind0306@gmail.com>
2026-02-18 09:54:31 +0900

    feat: add transparent background to neovim colorscheme
    - clear guibg for 8 highlight groups (Normal, NormalNC, NonText, SignColumn, EndOfBuffer, LineNr, CursorLineNr, FoldColumn) to inherit Ghostty's background-opacity

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,5 +4,22 @@ return {
   priority = 1000,
   config = function()
     vim.cmd("colorscheme iceberg")
+
+    -- Enable transparent background (inherits Ghostty's background-opacity)
+    local transparent_groups = {
+      "Normal",
+      "NormalNC",
+      "NonText",
+      "SignColumn",
+      "EndOfBuffer",
+      "LineNr",
+      "CursorLineNr",
+      "FoldColumn",
+    }
+    for _, group in ipairs(transparent_groups) do
+      local hl = vim.api.nvim_get_hl(0, { name = group })
+      hl.bg = nil
+      vim.api.nvim_set_hl(0, group, hl)
+    end
   end,
 }
