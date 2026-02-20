8fb8e052df2147cca3417e2f0452bfb9d92fcadd
Takayyz <like.the.wind0306@gmail.com>
2026-02-20 23:54:56 +0900

    feat: add keymaps for search highlight, pane split, and tab
    - add Esc Esc to clear search highlight
    - add Leader - / \ for horizontal / vertical pane split
    - add Leader n for new file and Leader t for new tab

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,6 +11,7 @@ map("i", "jj", "<Esc>", { desc = "Escape" })
 -----------------------------------
 -- Normal mode
 -----------------------------------
+map("n", "<Esc><Esc>", "<Cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 map("n", "<Leader>w", "<Cmd>w<CR>", { desc = "Save" })
 map("n", "<Leader>q", "<Cmd>q<CR>", { desc = "Quit" })
 map("n", "<Leader>h", "^", { desc = "Go to beginning of line" })
@@ -18,6 +19,10 @@ map("n", "<Leader>l", "$", { desc = "Go to end of line" })
 map("n", "<Leader>c", "gcc", { remap = true, desc = "Toggle comment" })
 map("n", "<Leader><", "<<", { desc = "Dedent line" })
 map("n", "<Leader>>", ">>", { desc = "Indent line" })
+map("n", "<Leader>-", "<Cmd>split<CR>", { desc = "Split pane horizontally" })
+map("n", "<Leader>\\", "<Cmd>vsplit<CR>", { desc = "Split pane vertically" })
+map("n", "<Leader>n", "<Cmd>enew<CR>", { desc = "New file" })
+map("n", "<Leader>t", "<Cmd>tabnew<CR>", { desc = "New tab" })
 
 -----------------------------------
 -- Visual mode
