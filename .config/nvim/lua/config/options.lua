a40fc3a79f67c6f747a615912b2bce5b11d92da8
Takayyz <like.the.wind0306@gmail.com>
2026-02-20 23:45:24 +0900

    feat: add neovim options for search, clipboard, and display
    - show invisible characters with listchars and highlight trailing whitespace
    - add search options (ignorecase, smartcase, incsearch, hlsearch)
    - set clipboard to sync with OS clipboard
    - set completeopt for better completion menu behavior
    - disable line wrapping

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,7 +4,42 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
+vim.opt.wrap = false
+vim.opt.list = true
+vim.opt.listchars = "tab:│─,trail:_,extends:»,precedes:«,nbsp:･"
+-- highlight trailing whitespace
+vim.api.nvim_set_hl(0, "TrailingWhitespace", { bg = "#e27878" })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("TrailingWhitespace", { clear = true }),
+  callback = function()
+    if vim.bo.buftype == "" then
+      vim.cmd([[match TrailingWhitespace /\s\+$/]])
+    end
+  end,
+})
 -- clear statusline
 vim.opt.laststatus = 0
 vim.opt.statusline = "─"
 vim.opt.fillchars:append({ stl = "─", stlnc = "─" })
+
+-----------------------------------
+-- Completion
+-----------------------------------
+vim.opt.completeopt = "menu,menuone,noinsert"
+
+-----------------------------------
+-- Search
+-----------------------------------
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+-----------------------------------
+-- Clipboard
+-----------------------------------
+if vim.fn.has("unnamedplus") == 1 then
+  vim.opt.clipboard = "unnamed,unnamedplus"
+else
+  vim.opt.clipboard = "unnamed"
+end
