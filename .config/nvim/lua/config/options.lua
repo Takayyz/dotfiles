2098b9d7911bc133592f72c01a06ecd70cb2d339
Takayyz <like.the.wind0306@gmail.com>
2026-02-22 11:15:37 +0900

    fix: enforce space indentation for php filetype
    - add explicit expandtab to php autocmd to override ftplugin defaults

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -46,6 +46,7 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "php",
   callback = function()
+    vim.opt_local.expandtab = true
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
     vim.opt_local.softtabstop = 4
