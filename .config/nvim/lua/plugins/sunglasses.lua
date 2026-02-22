a29ccf44a97a04922fc6fc98f467e3f291608515
Takayyz <like.the.wind0306@gmail.com>
2026-02-22 17:32:01 +0900

    feat: add sunglasses.nvim to dim inactive windows

---
+++ .config/nvim/lua/plugins/sunglasses.lua
@@ -0,0 +1,10 @@
+-- Dim inactive windows to visually highlight the focused pane
+-- https://github.com/miversen33/sunglasses.nvim
+return {
+  "miversen33/sunglasses.nvim",
+  event = "UIEnter",
+  opts = {
+    filter_type = "SHADE",
+    filter_percent = 0.7,
+  },
+}
