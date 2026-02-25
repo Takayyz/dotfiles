0f0b8954bd1c7823c59b9bb85aa977fe566c8207
Takayyz <like.the.wind0306@gmail.com>
2026-02-25 23:24:30 +0900

    feat: disable backtick auto-pairing in markdown files

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -7,4 +7,14 @@ return {
       map = "<M-e>",
     },
   },
+  config = function(_, opts)
+    local npairs = require("nvim-autopairs")
+    npairs.setup(opts)
+    -- Disable backtick auto-pairing in markdown
+    local Rule = require("nvim-autopairs.rule")
+    npairs.remove_rule("`")
+    npairs.add_rule(Rule("`", "`"):with_pair(function()
+      return vim.bo.filetype ~= "markdown"
+    end))
+  end,
 }
